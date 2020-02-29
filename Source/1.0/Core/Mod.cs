d9290264a0b7becec3d0cefe3a731fb789176f57
MrCool92 <nixa92@gmail.com>
2020-02-29 11:17:39 +0100

    changed Harmony as mod dependency
    added 1.1 missing files
    added 1.0 source files

---
+++ Source/1.0/Core/Mod.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using System.Reflection;
+using Verse;
+using UnityEngine;
+
+namespace AnimalsGenderOnCaravan
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            GetSettings<Settings>();
+            HarmonyInstance harmony = HarmonyInstance.Create("mrcool92.agoc");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            GetSettings<Settings>().DoWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "AGOC.AnimalsGenderOnCaravan".Translate();
+        }
+    }
+}
