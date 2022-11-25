9540f334178fb1b783b605a10e3a30daa622bb7c
zokradonh <az@zok.xyz>
2022-11-25 14:36:01 -0300

    Minor Cleanup

--- Source/1.4/HarmonyPatches/TransferableUtility_TransferAsOne_Patch.cs
+++ Source/1.4/HarmonyPatches/TransferableUtility_TransferAsOne_Patch.cs
@@ -5,7 +5,7 @@ using Verse;
 namespace AnimalsGenderOnCaravan
 {
     /// <summary>
-    /// Avoid Stacking of different training (only Attack training is checked)
+    /// Avoid stacking of animals with different stats 
     /// </summary>
     [HarmonyPatch(typeof(TransferableUtility), nameof(TransferableUtility.TransferAsOne))]
     public class TransferableUtility_TransferAsOne_Patch
@@ -13,35 +13,32 @@ namespace AnimalsGenderOnCaravan
         [HarmonyPrefix]
         public static bool Prefix(ref bool __result, Thing a, Thing b, TransferAsOneMode mode)
         {
-            if (a.def.category == ThingCategory.Pawn && b.def == a.def)
+            if (a.def.category != ThingCategory.Pawn || b.def != a.def) 
+                return true; // let original RimWorld decide here
+            
+            var pawn1 = (Pawn) a;
+            var pawn2 = (Pawn) b;
+            if (!pawn1.RaceProps.Animal) 
+                return true;
+            var attackTraining = TrainableDefOf.Release;
+            if (
+                pawn1.training.HasLearned(attackTraining) ^
+                pawn2.training.HasLearned(attackTraining)
+            )
             {
-                Pawn pawn1 = (Pawn) a;
-                Pawn pawn2 = (Pawn) b;
-                if (pawn1.RaceProps.Animal)
-                {
-                    var attackTraining = TrainableDefOf.Release;
-                    if (
-                        pawn1.training.HasLearned(attackTraining) ^
-                        pawn2.training.HasLearned(attackTraining)
-                        )
-                    {
-                        // if one has learned to be released for attacking and the other one not then avoid stacking
-                        __result = false;
-                        return false; // skip original
-                    }
+                // if one has learned to be released for attacking and the other one not then avoid stacking
+                __result = false;
+                return false;
+            }
 
-                    if (
-                        (pawn1.playerSettings?.followDrafted ?? false) ^
-                        (pawn2.playerSettings?.followDrafted ?? false)
-                        )
-                    {
-                        // if one has a caller and the other one not then avoid stacking
-                        __result = false;
-                        return false;
-                    }
-                    
-                        
-                }
+            if (
+                (pawn1.playerSettings?.followDrafted ?? false) ^
+                (pawn2.playerSettings?.followDrafted ?? false)
+            )
+            {
+                // if one is set to follow drafted master and the other one not then avoid stacking
+                __result = false;
+                return false;
             }
 
             return true;
