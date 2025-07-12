d7071dbf28a4e7edaf58873e1e9e38bf8cbae959
MrCool92 <nixa92@gmail.com>
2025-07-12 10:44:47 +0200

    1.6 update support

---
+++ Source/1.6/Core/Mod.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace AnimalsGenderOnCaravan
+{
+    public class Mod : Verse.Mod
+    {
+        private static Harmony harmony;
+        private static string id = "mrcool92.agoc";
+
+        public Mod(ModContentPack content) : base(content)
+        {
+            harmony = new Harmony(id);
+            harmony.PatchAll();
+        }
+        
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            GetSettings<Settings>().DoWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "AGOC.AnimalsGenderOnCaravan".Translate();
+        }
+    }
+}
---
+++ Source/1.6/HarmonyPatches/TransferableUtility_TransferAsOne_Patch.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace AnimalsGenderOnCaravan
+{
+    /// <summary>
+    /// Avoid stacking of animals with different stats 
+    /// </summary>
+    [HarmonyPatch(typeof(TransferableUtility), nameof(TransferableUtility.TransferAsOne))]
+    public class TransferableUtility_TransferAsOne_Patch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref bool __result, Thing a, Thing b, TransferAsOneMode mode)
+        {
+            if (a.def.category != ThingCategory.Pawn || b.def != a.def) 
+                return true; // let original RimWorld decide here
+            
+            Pawn pawn1 = (Pawn) a;
+            if (pawn1.RaceProps.Animal)
+            {
+                Pawn pawn2 = (Pawn) b;
+                TrainableDef attackTraining = TrainableDefOf.Release;
+                if (
+                    (pawn1.training?.HasLearned(attackTraining) ?? false) ^
+                    (pawn2.training?.HasLearned(attackTraining) ?? false)
+                )
+                {
+                    // if one has learned to be released for attacking and the other one not then avoid stacking
+                    __result = false;
+                    return false;
+                }
+
+                if (
+                    (pawn1.playerSettings?.followDrafted ?? false) ^
+                    (pawn2.playerSettings?.followDrafted ?? false)
+                )
+                {
+                    // if one is set to follow drafted master and the other one not then avoid stacking
+                    __result = false;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
